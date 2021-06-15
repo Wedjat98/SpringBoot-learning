56e32ac297f450900d99e9157e7c215a56f0187b
lionlemon <ame9807@gmail.com>
2021-06-15 23:25:56 +0900

    一个快速的HelloWorld

--- src/main/java/com/lionlemon/Application.java
+++ src/main/java/com/lionlemon/Application.java
@@ -1,5 +1,4 @@
-package application;
-
+package com.lionlemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/lionlemon/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.lionlemon.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //@Controller+@ResponseBody
+@RequestMapping("hello")
+public class HelloController {
+    //hello/boot
+    @GetMapping("boot")
+    public String SayHi(){
+        return "hello spring boot~~~~";
+    }
+}
