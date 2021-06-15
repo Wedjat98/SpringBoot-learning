da6d0c923022d697b288ca5a5b4d30af961c6728
lionlemon <ame9807@gmail.com>
2021-06-15 22:49:07 +0900

    hello SpringBoot!

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,11 @@
+package application;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication//标记成Springboot的应用
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
